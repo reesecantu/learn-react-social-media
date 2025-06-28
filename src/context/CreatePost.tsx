51d4a91e139ad0e49edcca4de2bb8fa46f936529
reesecantu <65140498+reesecantu@users.noreply.github.com>
2025-06-28 12:13:35 -0400

    ADD basic create post functionality

---
+++ src/context/CreatePost.tsx
@@ -0,0 +1,96 @@
+import { useState, type ChangeEvent } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { supabase } from "../supabase-client";
+
+interface PostInput {
+  title: string;
+  content: string;
+}
+
+const createPost = async (post: PostInput, imageFile: File) => {
+  const filePath = `${post.title}-${Date.now}-${imageFile.name}`;
+
+  const { error: uploadError } = await supabase.storage
+    .from("post-images")
+    .upload(filePath, imageFile);
+  if (uploadError) throw new Error(uploadError.message);
+
+  const { data: publicUrlData } = supabase.storage
+    .from("post-images")
+    .getPublicUrl(filePath);
+
+  const { data, error } = await supabase
+    .from("posts")
+    .insert([{ ...post, image_url: publicUrlData.publicUrl }]);
+  if (error) throw new Error(error.message);
+
+  return data;
+};
+
+export const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const { mutate } = useMutation({
+    mutationFn: (data: { post: PostInput; imageFile: File }) => {
+      return createPost(data.post, data.imageFile);
+    },
+  });
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!selectedFile) return;
+    mutate({
+      post: {
+        title,
+        content,
+      },
+      imageFile: selectedFile,
+    });
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setSelectedFile(e.target.files[0]);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {" "}
+      <div>
+        {" "}
+        <label>Title</label>
+        <input
+          type="text"
+          id="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        {" "}
+        <label>Content</label>
+        <textarea
+          id="content"
+          required
+          rows={5}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </div>
+      <div>
+        {" "}
+        <label>Upload Image</label>
+        <input
+          id="image_url"
+          type="file"
+          accept="image/*"
+          required
+          onChange={handleFileChange}
+        />
+      </div>
+      <button type="submit">Create Post</button>
+    </form>
+  );
+};
---
+++ src/pages/CreatePostPage.tsx
@@ -0,0 +1,9 @@
+import { CreatePost } from "../context/CreatePost";
+
+export const CreatePostPage = () => {
+  return <div>
+    {" "}
+    <h2>Create New Post</h2>
+    <CreatePost />
+  </div>;
+};
