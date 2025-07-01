1aa5d151938a53a731ec82ea3f2a97b8e3580a96
reesecantu <65140498+reesecantu@users.noreply.github.com>
2025-07-01 15:10:21 -0400

    Update Vite Base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/learn-react-social-media"
+  base: process.env.VITE_BASE_PATH || "/"
 })
